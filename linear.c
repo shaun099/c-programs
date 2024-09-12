3e392c7ddb76a6c7180b2f744b72a6374dd36204
Shaun MR <shaunmr009@gmail.com>
2024-09-12 07:24:56 +0530

    Linear_Search

--- linear.c
+++ linear.c
@@ -2,24 +2,27 @@
 void main()
 {
 	int i, num[100], n, search, flag = 0, d;
-	printf("enter the number of elements:");
+	printf("enter the number of elements:"); // to enter the elements in array.
 	scanf("%d", &n);
 	printf("enter the values:\n");
 	for (i = 0; i < n; i++)
 	{
 		scanf("%d", &num[i]);
 	}
-	printf("enter the value to be searched :");
+	printf("enter the value to be searched :"); // the value that we want to search
 	scanf("%d", &search);
 	for (i = 0; i < n; i++)
 	{
+		// searching operation
 		if (search == num[i])
 		{
 			flag = 1;
 			d = i + 1;
 			break;
+			// when search is found program breaks
 		}
 	}
+	// printing the output
 	if (flag == 1)
 	{
 		printf("%d is present at position:%d in the list", search, d);
