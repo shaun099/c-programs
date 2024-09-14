5214a9e7faeec92cf6b2ae5ff37bce2d72c5d20d
Shaun MR <shaunmr009@gmail.com>
2024-09-14 23:16:10 +0530

    Polynomial-Representation

--- poly.c
+++ poly.c
@@ -1,3 +1,18 @@
+/**
+ * This program allows the user to input a polynomial expression term by term,
+ * and then prints out the polynomial expression in a human-readable format.
+ *
+ * Example usage:
+ * Enter the number of terms in the polynomial: 3
+ * Enter the exponential of term 1: 2
+ * Enter the coefficient of term 1: 3
+ * Enter the exponential of term 2: 1
+ * Enter the coefficient of term 2: 2
+ * Enter the exponential of term 3: 0
+ * Enter the coefficient of term 3: 1
+ * Your polynomial expression is:
+ * 3(X^2)+2(X^1)+1(X^0)
+ */
 #include <stdio.h>
 #define MAX 10
 int main()
@@ -17,7 +32,7 @@ int main()
         printf("enter the coefficient of term %d:", i + 1);
         scanf("%d", &polyn1[i].coefficient);
     }
-
+    // printing the polynomial expression
     printf("your polynomial expression is:\n");
     for (i = 0; i < n1; i++)
     {
@@ -27,4 +42,5 @@ int main()
             printf("+");
         }
     }
+    return 0;
 }
