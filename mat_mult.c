ca843ea6cb184fc5ae9c5ed2a0283ed36864c5b3
Shaun MR <shaunmr009@gmail.com>
2024-09-23 18:27:54 +0530

    Matrix-Multiplication

--- mat_mult.c
+++ mat_mult.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+
+// Function to perform matrix multiplication
 void main()
 {
-	int r1, r2, c1, c2, i, j, k, sum = 0, a[10][10], b[10][10], c[10][10];
+	int r1, r2, c1, c2, i, j, k, sum = 0;
+	int a[10][10], b[10][10], c[10][10]; // Declare 3 2D arrays to store matrices
+
+	// Input for 1st matrix
 	printf("1st matrix\n");
 	printf("enter number of rows:");
 	scanf("%d", &r1);
@@ -12,11 +17,12 @@ void main()
 	{
 		for (j = 0; j < c1; j++)
 		{
-			printf("eneter the value a%d%d=", i, j);
-			scanf("%d", &a[i][j]);
+			printf("enter the value a%d%d=", i, j);
+			scanf("%d", &a[i][j]); // Store values in matrix a
 		}
 	}
-	// print matrix one
+
+	// Print matrix 1
 	printf("MATRIX 1");
 	for (i = 0; i < r1; i++)
 	{
@@ -27,11 +33,16 @@ void main()
 		}
 	}
 	printf("\n");
-	printf("\n2nd matrix\n");
+	printf("\n");
+
+	// Input for 2nd matrix
+	printf("2nd matrix\n");
 	printf("enter number of rows:");
 	scanf("%d", &r2);
 	printf("enter number of columns:");
 	scanf("%d", &c2);
+
+	// Check if matrix multiplication is possible
 	if (c1 != r2)
 	{
 		printf("ACTION CANNOT BE PERFORMED!!!!");
@@ -44,10 +55,11 @@ void main()
 			for (j = 0; j < c2; j++)
 			{
 				printf("enter the value b%d%d=", i, j);
-				scanf("%d", &b[i][j]);
+				scanf("%d", &b[i][j]); // Store values in matrix b
 			}
 		}
-		// print matrix 2
+
+		// Print matrix 2
 		printf("\nMATRIX 2:\n");
 		for (i = 0; i < r2; i++)
 		{
@@ -58,21 +70,21 @@ void main()
 			}
 		}
 
-		// multiplication
+		// Perform matrix multiplication
 		for (i = 0; i < r1; i++)
 		{
 			for (j = 0; j < c2; j++)
 			{
+				sum = 0; // Reset sum for each element
 				for (k = 0; k < r2; k++)
 				{
-					sum = sum + a[i][k] * b[k][j];
+					sum = sum + a[i][k] * b[k][j]; // Calculate dot product
 				}
-				c[i][j] = sum;
-				sum = 0;
+				c[i][j] = sum; // Store result in matrix c
 			}
 		}
-		printf("\n");
-		// print resultant matrix
+
+		// Print resultant matrix
 		printf("\nRESULTANT MATRIX:\n");
 		for (i = 0; i < r1; i++)
 		{
